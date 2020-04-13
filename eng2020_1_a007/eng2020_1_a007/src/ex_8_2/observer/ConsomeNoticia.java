d5aab2fb6651e23c4df970b48fb2094f4e1b5479
leopdani <51030652+leopdani@users.noreply.github.com>
2020-04-13 17:20:57 -0300

    Delete ConsomeNoticia.java

--- eng2020_1_a007/eng2020_1_a007/src/ex_8_2/observer/ConsomeNoticia.java
+++
@@ -1,8 +0,0 @@
-package ex_8_2.observer;
-
-/*esta interface é a interface observer */
-
-public interface ConsomeNoticia {
-	public void consomeNoticia(String textoNoticia, int dia, int mes, 
-			String topico);
-}
