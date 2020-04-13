a1a5dfd7c9c0429f94cdaf1a2c2437c874d258a4
leopdani <51030652+leopdani@users.noreply.github.com>
2020-04-13 16:50:10 -0300

    Add files via upload

---
+++ eng2020_1_a007/src/ex_8_2/TesteNoticias.java
@@ -0,0 +1,62 @@
+package ex_8_2;
+
+
+import ex_8_2.observer.AgregadorMes;
+import ex_8_2.observer.AgregadorTopico;
+import ex_8_2.observer.Agregadores;
+import ex_8_2.observer.NoticiarioAssina;
+import ex_8_2.observer.Publicador;
+import ex_8_2.subject.FonteNoticias;
+import ex_8_2.subject.Noticia;
+
+public class TesteNoticias {
+
+	public static void main(String[] args) {
+		
+		FonteNoticias fonte = new FonteNoticias();
+		
+		Publicador publicador = new Publicador(fonte);
+		publicador.assina();	
+		
+		NoticiarioAssina observador1 = new NoticiarioAssina(fonte, "Estadao");	
+		observador1.setTopicos("Fake News");
+		observador1.assina();
+		
+		NoticiarioAssina observador2 = new NoticiarioAssina(fonte, "Folha");	
+		observador2.setTopicos("Futebol");
+		observador2.assina();
+		
+		Agregadores agregadorTopico = new AgregadorTopico(fonte, "Fake News");
+		agregadorTopico.assina();
+		Agregadores agregadorMes = new AgregadorMes(fonte, 4);
+		agregadorMes.assina();	
+		
+		System.out.println();
+		Noticia noticia = new Noticia();
+
+		
+		for(int i = 1; i < 12; i++) {
+			noticia.setTexto("Notícia de teste " + i + "\n");
+			noticia.setDia(i);
+			noticia.setMes(4);
+			noticia.setTopico("Fake News");
+			if(i == 4) {
+				System.out.println();
+				observador1.cancela();
+				System.out.println();
+			}
+			fonte.setNoticia(noticia);
+			System.out.println("Fim do ciclo\n");
+		}
+		
+		noticia.setTexto("Jogo do palmeiras vs corintias termina em 6 a 0 para o verdão!");
+		noticia.setDia(1);
+		noticia.setMes(5);
+		noticia.setTopico("Futebol");
+		fonte.setNoticia(noticia);
+		
+		
+
+	}
+
+}
---
+++ eng2020_1_a007/src/ex_8_2/observer/AgregadorTopico.java
@@ -0,0 +1,48 @@
+package ex_8_2.observer;
+
+import java.util.ArrayList;
+
+import ex_8_2.subject.FonteNoticias;
+import ex_8_2.subject.Noticia;
+
+public class AgregadorTopico extends Agregadores{
+	
+	private ArrayList<Noticia> noticias = new ArrayList<Noticia>();
+	private String topico;
+	
+	public AgregadorTopico(FonteNoticias fonte, String topico) {
+		this.topico = topico;
+		this.fonte = fonte;
+		this.nome = "Agrupador de topicos";
+	}
+	
+	@Override
+	public void concatena(Noticia noticia) {
+		if(topico.equals(noticia.getTopico())){
+			noticias.add(noticia);			
+		}
+		if(noticias.size() == 10) {
+			System.out.println("---------------------");
+			System.out.println("Agregador de topicos enviando notícias.\n");
+			System.out.printf("Data da última notícia: %d/%d \n", noticias.get(noticias.size()-1).getDia(), noticias.get(noticias.size()-1).getMes());
+			System.out.println("------------------------");
+			for(Noticia cnoticia: noticias) {
+				System.out.printf("Noticia: %s\n", cnoticia.getTexto());
+			}
+			System.out.println("------------------");
+			removeNoticias();
+		}
+	}
+	
+	private void removeNoticias() {
+		noticias.clear();
+	}
+
+	@Override
+	public void consomeNoticia(String textoNoticia, int dia, int mes, String topico) {
+		Noticia noticia = new Noticia(textoNoticia, dia, mes, topico);
+		concatena(noticia);
+	}
+
+
+}
